521bb53f092734ec10bf0e100d16e149af39fdd3
RealMrHex <RealMrHex@gmail.com>
2023-12-05 00:35:14 +0330

    Session 2

--- routes/episodes/1.introduction.php
+++ routes/episodes/1.introduction.php
@@ -4,5 +4,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('1', static function ()
 {
-    return 'Silence is golden';
+    return 'Episode 1, Session 1: Silence is golden';
+});
+
+Route::get('2', static function ()
+{
+    return 'Episode 1, Session 2: Silence is golden';
 });
