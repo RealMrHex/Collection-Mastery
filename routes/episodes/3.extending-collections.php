4e8f7613392623dd953212b624720821084bdf18
RealMrHex <RealMrHex@gmail.com>
2023-12-05 03:52:25 +0330

    Session 5

--- routes/episodes/3.extending-collections.php
+++ routes/episodes/3.extending-collections.php
@@ -1,8 +1,19 @@
 <?php
 
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Route;
 
 Route::get('5', static function ()
 {
+    // todo: Add this in the service provider
+    Collection::macro('toUpper', function ()
+    {
+        return $this->map(fn($item) => strtoupper($item));
+    });
 
+    $colors = ['red', 'green', 'blue'];
+
+    $collection = collect($colors)->toUpper();
+
+    dd($colors, $collection);
 });
