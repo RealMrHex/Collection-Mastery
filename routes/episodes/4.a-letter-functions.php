332e0ba1a9d3062d2c78cddbafa4563836e9e99a
RealMrHex <RealMrHex@gmail.com>
2023-12-06 10:36:56 +0330

    Session 8

--- routes/episodes/4.a-letter-functions.php
+++ routes/episodes/4.a-letter-functions.php
@@ -9,3 +9,29 @@ Route::get('7', static function ()
     $all        = $collection->all();
     dd($colors, $collection, $all);
 });
+
+Route::get('8', static function ()
+{
+    $points    = [18, 16, 20, 19];
+    $pointsAvg = collect($points)->average();
+    #$pointsAvg = collect($points)->avg(); // note: avg alias
+
+    $students = [
+        [
+            'name'  => 'John',
+            'point' => 19,
+        ],
+        [
+            'name'  => 'Sarah',
+            'point' => 20,
+        ],
+        [
+            'name'  => 'Rose',
+            'point' => 15,
+        ],
+    ];
+    $studentsAvg = collect($students)->average('point');
+    #$studentsAvg = collect($students)->avg('point');  // note: avg alias
+
+    dd($points, $pointsAvg, $students, $studentsAvg);
+});
