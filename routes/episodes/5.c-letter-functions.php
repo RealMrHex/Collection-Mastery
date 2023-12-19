63fd9c4df238fa129a24dc8cd8d77bc4c5239c56
RealMrHex <RealMrHex@gmail.com>
2023-12-19 13:56:40 +0330

    session 10 - chunkWhile

--- routes/episodes/5.c-letter-functions.php
+++ routes/episodes/5.c-letter-functions.php
@@ -4,9 +4,33 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('9', static function ()
 {
-    $colors = collect(['red', 'green', 'blue', 'yellow', 'black', 'white', 'rose', 'amber']);
+    $colors        = collect(['red', 'green', 'blue', 'yellow', 'black', 'white', 'rose', 'amber']);
     $chunkedColors = $colors->chunk(4);
 
     dd($chunkedColors->all());
 });
+
+Route::get('10', static function ()
+{
+    $lettersArray   = str_split('AABBBCCCCDDDDDE');
+    $letters        = collect($lettersArray);
+    $chunkedLetters = $letters->chunkWhile(
+        function ($currentValue, $currentIndex, $previousChunk)
+        {
+            return ($currentValue === $previousChunk->last());
+        }
+    );
+
+    $names = collect(['Ali', 'Reza', 'Mohammad', 'And', 'Saeed', 'Qolam', 'And', 'Niaz']);
+    $chunkedNames = $names->chunkWhile(
+        function ($currentValue)
+        {
+            return ($currentValue !== 'And');
+        }
+    );
+
+    dd($lettersArray, $letters, $chunkedLetters->all(), $chunkedNames);
+});
+
+
 
