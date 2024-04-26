55cd594e67996980e78ec3338c433361c7f9ea5b
RealMrHex <RealMrHex@gmail.com>
2024-04-26 22:27:59 +0330

    diff method

--- routes/episodes/6.d-letter-functions.php
+++ routes/episodes/6.d-letter-functions.php
@@ -11,3 +11,27 @@ Route::get('21', static function ()
 
     $collection->dd();
 });
+
+Route::get('22', static function ()
+{
+    $numbers = collect([1, 2, 3, 4, 5, 6]);
+    $result  = $numbers->diff([2, 4, 6]);
+
+    $data = collect(
+        [
+            'armin' => 'Apple',
+            'negar' => 'Orange',
+            'sevda' => 'Pineapple'
+        ]
+    );
+
+    $dataResult = $data->diff(
+        [
+            'pineapple',
+            'Apple',
+            'Orange',
+        ]
+    );
+
+    dd($result->all(), $dataResult->all());
+});
